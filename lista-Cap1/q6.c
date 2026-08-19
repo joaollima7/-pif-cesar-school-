7dac229986bf4d9cd4100b56aab85a229ec7e675
João Lima <joaobr171@gmail.com>
2026-08-19 02:40:54 -0300

    Update q6.c

--- lista-Cap1/q6.c
+++ lista-Cap1/q6.c
@@ -1,6 +1,6 @@
 main() // faltou especificar o tipo main como int //
 {
-int a=1; b=2; c=3: // As variaves foram separadas por ponto e virgula sem declarar o tipo int, e alem disso no final da linha foi colocado dois pontos ao inves de ponto e virgula
+int a=1; b=2; c=3: // As variaves foram separadas por ponto e virgula sem declarar o tipo int, e alem disso no final da linha foi colocado dois pontos ao inves de ponto e virgula //
 printf("0s números são: %d%d%d\n, a, b, c, d); // Faltou fechar as aspas duplas //
 system("pause");
 }
