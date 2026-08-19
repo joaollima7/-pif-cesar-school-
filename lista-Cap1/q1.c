c05494c616c683e208803fdc3862be30550cca3f
João Lima <joaobr171@gmail.com>
2026-08-19 01:23:34 -0300

    .

---
+++ lista-Cap1/q1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main() {
+    SetConsoleOutputCP(65001);
+    int anoNascimento = 2008;
+    printf("O ano de nascimento é: %d\n", anoNascimento);
+    return 0;
+}
