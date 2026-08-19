3a175f0432cd487463a3f69daee473b9e6b1bda5
João Lima <joaobr171@gmail.com>
2026-08-19 01:35:32 -0300

    Update q2.c

--- lista-Cap1/q2.c
+++ lista-Cap1/q2.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+ 
+int main (){ 
+    SetConsoleOutputCP(CP_UTF8);
+    float pi = 3.14159f;
+    printf("O valor de pi é: %.3f\n", pi);
+    return 0;
+ }
