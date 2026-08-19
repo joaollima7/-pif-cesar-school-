abef7a57e1af2ef39325d327f6443e796ee287e3
João Lima <joaobr171@gmail.com>
2026-08-19 14:06:13 -0300

    Create q7.c

---
+++ lista-Cap1/q7.c
@@ -0,0 +1,14 @@
+printf("\n\tBom dia! Shirley.");  // Uma linha em branco no inicio por conta do \n e um tab no inicio da frase por conta do \t // Bom dia! Shirley.
+printf("Você já tomou café? \n"); // O texto tem um espaço no final e uma quebra de linha por conta do \n // Você já tomou café?
+
+printf("\n\nA solução não existe!\nNão insista."); // Duas linhas em branco por conta do \n\n e uma quebra de linha no final por conta do \n //!
+A solução não existe!
+Não insista.
+printf("Duas\tlinhas\tde\tsaída\nou\tuma?"); // O texto tem tabulações por conta do \t e uma quebra de linha no meio por conta do \n // 
+ Duas	linhas	de	saída
+ou	uma?
+
+printf("%s\n%s\n%s\n", "um", "dois", "três"); // Cada string inserido no no seu lugar por conta do %s e uma quebra de linha no final de cada string por conta do \n // 
+um
+dois
+três
