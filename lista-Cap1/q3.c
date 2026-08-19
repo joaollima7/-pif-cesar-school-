d86f1bf03e6b736c6ef44390cdccd30c1f7365ad
João Lima <joaobr171@gmail.com>
2026-08-19 01:52:44 -0300

    Update q3.c

--- lista-Cap1/q3.c
+++ lista-Cap1/q3.c
@@ -1,1 +1,18 @@
+/* Esse programa exibe uma mensagem de boas-vindas ao aluno
+* e mostra tambem o uso de comentarios em uma unica linha
+*
+* Terceiro programa
+***************************************************************/
 
+/* Prog3.C */
+
+
+#include <stdio.h> /* Para printf() */
+#include <stdlib.h>/* Para system() */
+
+int main()       /* Função main / Inicio do programa */
+{ /* início do corpo da função main */
+    printf("Bem-vindo a aula de Programacao!"); /* Chamada à função print para exibir mensagem */
+    system("PAUSE");                           /* Chamada à função system para pausar a tela*/
+    return 0;
+}/* Fim do corpo da função main / Encerra o programa  */
