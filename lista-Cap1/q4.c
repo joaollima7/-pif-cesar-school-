2f53003cdbe103458c70353f9016d5fbbc6e1409
João Lima <joaobr171@gmail.com>
2026-08-19 02:02:43 -0300

    Create q4.c

---
+++ lista-Cap1/q4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>; /* Ponto e virgula na diretiva */
+int Main{} /* Trocou os parenteses por chaves*/ /*main escrito em maiusculo*/
+(
+printf( Existem %d semanas no ano.,52); /* Falta de aspas duplas*/
+cout << endl; /* Não existe em C */
+system("PAUSE");
+return O; /*Foi digitado a letra o em maiusculo ao inves do número 0*/
+)
+
+/* Código certo*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    printf("Existem %d semanas no ano.", 52);
+    system("PAUSE");
+    return 0;
+}
