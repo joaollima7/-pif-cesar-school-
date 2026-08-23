fedf180d32748bd9792e9ccef9e4841f24613810
João Lima <joaobr171@gmail.com>
2026-08-23 14:32:40 -0300

    Update q07.c

--- lista-Cap1/q07.c
+++ lista-Cap1/q07.c
@@ -1,12 +1,10 @@
-Questão 07.
-
-Resposta:
-
-a) printf("\n\tBom dia! Shirley.");
+Questão 07:
+    
+a)printf("\n\tBom dia! Shirley.");
 
 
     Bom dia! Shirley.
-b) printf("Você já tomou café? \n");
+b)printf("Você já tomou café? \n");
 
 Você já tomou café? 
 
@@ -16,11 +14,11 @@ c) printf("\n\nA solução não existe!\nNão insista.");
 
 A solução não existe!
 Não insista.
-d) printf("Duas\tlinhas\tde\tsaída\nou\tuma?");
+d)printf("Duas\tlinhas\tde\tsaída\nou\tuma?");
 
 Duas    linhas  de  saída
 ou  uma?
-e) printf("%s\n%s\n%s\n", "um", "dois", "três");
+e)printf("%s\n%s\n%s\n", "um", "dois", "três");
 
 um
 dois
