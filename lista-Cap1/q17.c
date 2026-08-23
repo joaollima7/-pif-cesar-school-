9ae67592e81a07f6a884520c00bf641b50e55bca
João Lima <joaobr171@gmail.com>
2026-08-23 16:18:21 -0300

    Update q17.c

--- lista-Cap1/q17.c
+++ lista-Cap1/q17.c
@@ -1,1 +1,3 @@
-
+Questão 17:
+  A, B e C estão corretas, já a letra D falta os parenteses que sao obrigatorios pra chamar uma funçãp em C.
+Isso demonstro que em C ignora espaços em branco fora das aspas.
