2f66bb11ac40a3980a72bcf713ed6c8f73c60d12
João Lima <joaobr171@gmail.com>
2026-08-23 14:25:29 -0300

    Update q07.c

--- lista-Cap1/q07.c
+++ lista-Cap1/q07.c
@@ -1,14 +1,23 @@
-printf("\n\tBom dia! Shirley.");  // Uma linha em branco no inicio por conta do \n e um tab no inicio da frase por conta do \t // Bom dia! Shirley.
-printf("Você já tomou café? \n"); // O texto tem um espaço no final e uma quebra de linha por conta do \n // Você já tomou café?
+a) printf("\n\tBom dia! Shirley.");
 
-printf("\n\nA solução não existe!\nNão insista."); // Duas linhas em branco por conta do \n\n e uma quebra de linha no final por conta do \n //!
+
+   Bom dia! Shirley.
+b) printf("Você já tomou café? \n");
+
+Você já tomou café? 
+
+c) printf("\n\nA solução não existe!\nNão insista.");
+
+
+
 A solução não existe!
 Não insista.
-printf("Duas\tlinhas\tde\tsaída\nou\tuma?"); // O texto tem tabulações por conta do \t e uma quebra de linha no meio por conta do \n // 
- Duas	linhas	de	saída
-ou	uma?
+d) printf("Duas\tlinhas\tde\tsaída\nou\tuma?");
 
-printf("%s\n%s\n%s\n", "um", "dois", "três"); // Cada string inserido no no seu lugar por conta do %s e uma quebra de linha no final de cada string por conta do \n // 
+Duas    linhas  de  saída
+ou  uma?
+e) printf("%s\n%s\n%s\n", "um", "dois", "três");
+
 um
 dois
 três
