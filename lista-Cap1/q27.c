1c63847312e88b662d31d9a08c099c383ff92818
João Lima <joaobr171@gmail.com>
2026-08-25 20:15:42 -0300

    Update q27.c

--- lista-Cap1/q27.c
+++ lista-Cap1/q27.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+    int seg, h, m, s;
+
+    printf("Digite os segundos: ");
+    scanf("%d", &seg);
+
+    h = seg / 3600;
+    m = (seg % 3600) / 60;
+    s = seg % 60;
+
+    printf("%d segundos sao %d hora, %d minuto e %d segundos.\n", seg, h, m, s);
+
+    return 0;
+}
