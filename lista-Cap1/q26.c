31732a2f244d9fef28ca0052fd437bcdf8c49d49
João Lima <joaobr171@gmail.com>
2026-08-25 20:10:13 -0300

    Update q26.c

--- lista-Cap1/q26.c
+++ lista-Cap1/q26.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    printf("        *\n");
+    printf("       o/\\o\n");
+    printf("      o/  \\o\n");
+    printf("     o/*  *\\o\n");
+    printf("    o/+    +\\o\n");
+    printf("   o/*+    +*\\o\n");
+    printf("  o/ ******** \\o\n");
+    printf("       |  |\n");
+    printf("      [====]\n");
+
+    return 0;
+}
