3a8eabe78c1eeda8fb968ddf4f302421ac10e079
João Lima <joaobr171@gmail.com>
2026-08-25 19:52:15 -0300

    Update q21.c

--- lista-Cap1/q21.c
+++ lista-Cap1/q21.c
@@ -1,1 +1,33 @@
+Primeiro codigo:
+#include <stdio.h>
+#include <windows.h>
 
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    printf("Treinamento em programação.\nLinguagem C.\n");
+    return 0;
+}
+
+Segundo codigo:
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    printf("Treinamento em programação.\n");
+    printf("Linguagem C.\n");
+    return 0;
+}
+
+Terceiro codigo:
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    printf("+--------------------------------+\n");
+    printf("| Treinamento em programação.    |\n");
+    printf("| Linguagem C.                   |\n");
+    printf("+--------------------------------+\n");
+    return 0;
+}
