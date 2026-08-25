97e66a96cfab647a2a07ddfd3f76efbb92774a17
João Lima <joaobr171@gmail.com>
2026-08-25 20:24:48 -0300

    Update q28.c

--- lista-Cap1/q28.c
+++ lista-Cap1/q28.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <windows.h>
 
+int main() {
+    SetConsoleOutputCP (CP_UTF8);
+    int n1, n2, n3;
+    double media;
+
+    printf("Digite três números: ");
+    scanf("%d %d %d", &n1, &n2, &n3);
+
+    media = (n1 + n2 + n3) / 3.0;
+
+    printf("A média é: %.2lf\n", media);
+
+    return 0;
+}
